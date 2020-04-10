ad36084e7cc4963296f7eb64f6e6164ff92a6cd8
que2017 <1563645183@qq.com>
2020-04-11 07:19:59 +0800

    完成注销功能

--- src/components/welcome/welcome.vue
+++ src/components/welcome/welcome.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-		<h2>欢迎来到Estore商城！您可以：</h2>
+		<h2 v-show="!userChecked">欢迎来到Estore商城！您可以：</h2>
+		<h2 v-show="userChecked">欢迎回来{{username}}！您可以：</h2>
 		<div>
-			<el-button type="primary" @click="jumpToPage('login')">登陆</el-button>
-			<el-button @click="jumpToPage('regist')">注册</el-button>
+			<el-button v-show="!userChecked" type="primary" @click="jumpToPage('login')">登陆</el-button>
+			<el-button v-show="!userChecked" @click="jumpToPage('regist')">注册</el-button>
+			<el-button v-show="userChecked" @click="logout()">注销</el-button>
 		</div>
 	</div>
 </template>
@@ -12,14 +14,36 @@
     export default {
         data () {
             return {
-				
+				userChecked: false,
+				username: ''
 			}
         },
         methods: {
             jumpToPage(page) {
 				this.$router.push(page);
+			},
+			logout() {
+				this.$http.get('/Estore/servlet/LogoutServlet').then(res => {
+					this.$router.go(0);
+				}, err => {
+					this.$router.go(0);
+				})
 			}
-        }
+        },
+		created() {
+			console.log('created');
+			this.$http.get('/Estore/servlet/CheckSessionServlet').then(res => {
+				let resData = eval('(' + res.bodyText + ')');
+				if (resData.result === 'succ') {
+					this.userChecked = true;
+					this.username = resData.user.username;
+				} else {
+					this.userChecked = false;
+				}
+			}, err => {
+				this.userChecked = false;
+			});
+		}
     } 
 </script>
 
