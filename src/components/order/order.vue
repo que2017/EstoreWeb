ae9a178462e7272995053a9431e910ec1b52c289
que2017 <1563645183@qq.com>
2020-04-22 23:19:07 +0800

    完成创建订单界面的数据响应功能

--- src/components/order/order.vue
+++ src/components/order/order.vue
@@ -2,7 +2,7 @@
     <div class="order-wrapper">
 		<div>
 			<el-input style="width: 20rem;" type="textarea" :autosize="{minRows: 3}" placeholder="请输入地址" v-model="address"></el-input>
-			<el-button style="margin-left: 2rem;" type="primary">提交订单</el-button>
+			<el-button style="margin-left: 2rem;" type="primary" @click="submitOrder()">提交订单</el-button>
 		</div>
 		<div class="order-detail">
 			<span class="order-detail-head">商品详情</span>
@@ -29,7 +29,25 @@
 			} 
         },
         methods: {
-            
+            submitOrder() {
+				if (this.address === '') {
+					this.$message.error('请输入地址!');
+					return;
+				}
+				this.$http.get('/Estore/servlet/AddOrderServlet', {params:{'address': this.address}}).then(res => {
+					let data = eval('(' + res.bodyText + ')');
+					if (data.result === 'succ') {
+						this.$message({
+							message: '订单创建成功！',
+							type: 'success'
+						});
+						// 跳转支付界面
+					} else {
+						this.$message.error('订单创建失败，请稍后重试！');
+						this.$router.push('cartproduct');
+					}
+				});
+			}
         },
 		created() {
 			this.cartDataList = this.$store.state.cartproductData.cartData;
